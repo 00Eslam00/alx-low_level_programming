a88d7b1b9710e772bcc6caa8c2dc218683fa299c
Eslam <11410120210064@stud.cu.edu.eg>
2023-03-16 16:34:15 -0700

    es

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,16 +3,16 @@
 /**
  * main - entry point
  *
- * Description: A C program that prints a qoute using write function
- *	ssize_t write(int fd,const void *buf,size_t count)
+ * Description: print a qoute using write function
+ *    ssize_t write(int fd, const void *buf, size_t count);
  *
- * Return: 1 (error)
+ * Return: 1 (error).
 */
 
-int main(void)
-{
-	char qoute[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, qoute, 59);
+int main(void)
+{	char qou[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, qou, 59);
 	return (1);
 }
