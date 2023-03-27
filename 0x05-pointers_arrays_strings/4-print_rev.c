da3de50344e7fed66044be92763d46891072549d
Eslam <11410120210064@stud.cu.edu.eg>
2023-03-27 14:54:40 -0700

    new

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,21 +2,23 @@
 #include "main.h"
 
 /**
- * _puts - function
- * @str: param1
+ * print_rev - function
+ * @s: param1
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-void _puts(char *str)
+void print_rev(char *s)
 {
-	do {
-		if (!*str)
-			break;
 
-		_putchar(*str);
-	} while (*(++str));
-
+	while (*(s++))
+	{
+	}
+	s--;
+	while (*(--s))
+	{
+		_putchar(*s);
+	}
 	_putchar('\n');
 }
