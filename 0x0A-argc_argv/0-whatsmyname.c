e4cbc5d26bdadeba6dbc1132fd82bdaf0e27a976
Eslam <11410120210064@stud.cu.edu.eg>
2023-04-11 12:02:05 -0700

    new

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc : 1st param
+ * @argv : 2nd param
+ * Return: Always 0.
+ */
+
+int main(int argc, char const *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
