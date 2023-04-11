adb3145a468f1e8537aa697c2de0b6eeba37677c
Eslam <11410120210064@stud.cu.edu.eg>
2023-04-11 13:23:04 -0700

    file

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * free_grid - free the grid
+ * @grid : param
+ * @height : param
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
